577f444816731be115597bb45d2c8a65042bfdcd
LaithAlebrahim <96999016+LaithAlebrahim@users.noreply.github.com>
2022-06-11 16:34:54 +0300

    Add files via upload

---
+++ Frontend/src/home.js
@@ -0,0 +1,17 @@
+
+const Home = () => {
+    return ( 
+        <div id="homeid" class="home">
+             <div class="over-lay">
+             <div class="home-content">
+                 <h1 class="title">we are creative agency</h1>
+                 <p class="home-discription">“Learning to code is useful no matter what your career ambitions are.”</p>
+               
+             </div>
+            </div>
+         </div>
+        
+     );
+}
+ 
+export default Home;
---
+++ Frontend/src/navbar.js
@@ -0,0 +1,24 @@
+const navbar = () => {
+    return (
+
+        <header>
+        
+            <div class="header-conent">
+             
+                <nav class="navbar">
+                    <ul class="list-unstyle">
+                        <li><a href="#homeid" class="link-item">Home</a></li>
+                        <li><a href="#serviceid" class="link-item">Services</a></li>
+                        <li><a href="#freqid" class="link-item">QA</a></li>
+                        <li><a href="#contactid" class="link-item"> Contact Us</a></li>
+                    </ul>
+                </nav>
+               
+            </div>
+      
+    
+    </header>
+    )
+}
+ 
+export default navbar;

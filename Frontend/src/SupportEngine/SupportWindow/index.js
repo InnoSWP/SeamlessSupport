3a876ba1196c824bf3f79ecb845bbaf6cd345eaa
LaithAlebrahim <96999016+LaithAlebrahim@users.noreply.github.com>
2022-06-20 12:57:04 +0300

    I added the socket library and edit the props

--- Frontend/src/SupportEngine/SupportWindow/index.js
+++ Frontend/src/SupportEngine/SupportWindow/index.js
@@ -2,28 +2,32 @@ import React,{useState} from 'react'
 import { styles } from '../styles'
 import EmailForm from '../SupportWindow/EmailForm'
 import ChatEngine from './ChatEngine'
+import io from "socket.io-client";
 const SupportWindows = props => {
-    const[user,setUser]=useState(null)
-    const[chat,setChat]=useState(null)
+    const[Show,setShow]=useState(true)
+    const [email,setEmail]=useState("")
+    const socket = io.connect ("");
     return (
         <div  className='transition-5'
         style={{...{visibility:props.visible?'visible':'hidden'},...styles.supportWindow
         ,...{opacity:props.visible?'1':'0'}}}>
     
-        
-            
-     
-            <EmailForm 
-                visible={user === null || chat === null}
-                setUser={user => setUser(user)} 
-                setChat={chat => setChat(chat)} 
-            />
 
-            <ChatEngine 
-                visible={user !== null && chat !== null}
-                user={user} 
-                chat={chat} 
+            <EmailForm 
+               visible={Show===true}
+               setShow={show => setShow(show)} 
+                setEmail={email => setEmail(email)}
+           
             />
+            
+                  
+    <ChatEngine 
+               visible={Show===false}
+              email={email}
+              socket={socket}
+            />
+     
+         
         </div>
     )
 }

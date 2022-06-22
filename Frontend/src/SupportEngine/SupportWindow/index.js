38e6ffbe227eede148d6f6b89d9a25ca4be6920b
LaithAlebrahim <96999016+LaithAlebrahim@users.noreply.github.com>
2022-06-22 20:35:07 +0300

    i edit the socket library and adding our server

--- Frontend/src/SupportEngine/SupportWindow/index.js
+++ Frontend/src/SupportEngine/SupportWindow/index.js
@@ -2,11 +2,12 @@ import React,{useState} from 'react'
 import { styles } from '../styles'
 import EmailForm from '../SupportWindow/EmailForm'
 import ChatEngine from './ChatEngine'
-import io from "socket.io-client";
 const SupportWindows = props => {
     const[Show,setShow]=useState(true)
     const [email,setEmail]=useState("")
-    const socket = io.connect ("127.0.0.1:5000");
+   
+          const socket = require("socket.io-client")("http://127.0.0.1:5000/");
+      
     return (
         <div  className='transition-5'
         style={{...{visibility:props.visible?'visible':'hidden'},...styles.supportWindow
